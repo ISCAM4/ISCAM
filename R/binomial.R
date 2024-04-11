c30d6bbfd510e4fced0f68dfafe189206ff1051d
Visruth <vsrimath@calpoly.edu>
2024-04-11 08:47:07 -0700

    Added binomial functions
    Need to clean up some functions and write some better documentation

--- R/binomial.R
+++ R/binomial.R
@@ -108,7 +108,11 @@ binomnorm <- function(k, n, prob, direction) {
   cat(full, "\n")
 }
 
-#' Regection Region for Binomial
+#' Rejection Region for Binomial
+#'
+#' `binompower` determines the rejection region corresponding to the level of
+#'  significance and the first probability and shows the binomial distribution
+#'  shading its corresponding region.
 #'
 #' @param LOS A numeric value representing the level of significance
 #' @param n A numeric value representing the sample size
@@ -205,4 +209,283 @@ binompower <- function(LOS, n, prob1, alternative, prob2 = NULL) {
     title(newtitle)
   }
   par(mfrow = c(1, 1))
+}
+
+#' Calculate Binomial Tail Probabilities
+#'
+#' `binomprob` calculates the probability of the number of success of interest
+#'    using a binomial distribution and plots the distribution.
+#'
+#' @param k number of successes of interest.
+#' @param n number of trials.
+#' @param prob success probability. Numeric between 0 & 1.
+#' @param lower.tail Boolean for finding the probability above (FALSE) or below (TRUE) the inputted value (inclusive)
+#'
+#' @return The probability of the binomial distribution along with a graph of the distribution.
+#' @export
+#'
+#' @examples
+binomprob <- function(k, n, prob, lower.tail) {
+  Description <- "iscambinomprob(k, n, prob, lower.tail) \n This function calculates tail probabilities from the binomial distribution.\r
+k is the number of successes of interest (must be integer), n and prob are the number of trials and success probability \n lower.tail is a Boolean for finding the probability above (FALSE) or below (TRUE) the inputted value (inclusive)"
+  # TODO Stop if probability is wrong
+
+  if (as.character(k) == "?") stop(Description)
+  par(mar = c(4, 3, 2, 2))
+  thisx <- 0:n
+  minx <- max(0, n * prob - 4 * sqrt(prob * (1 - prob) * n))
+  maxx <- min(n, n * prob + 4 * sqrt(prob * (1 - prob) * n))
+  maxx <- max(k + 1, maxx)
+  #   myy=dbinom(floor(n*prob), n, prob)/2
+  myy <- dbinom(floor(n * prob), n, prob)
+  plot(thisx, dbinom(thisx, size = n, prob), xlab = " ", ylab = " ", type = "h", xlim = c(minx, maxx), panel.first = grid(), lwd = 2)
+  abline(h = 0, col = "gray")
+
+  if (lower.tail) {
+    this.prob <- pbinom(k, n, prob)
+    showprob <- format(this.prob, digits = 4)
+    lines(0:k, dbinom(0:k, size = n, prob), col = "red", type = "h", lwd = 2)
+    text((minx + n * prob) / 4, myy, labels = paste("P(X\u2264", k, ")\n =", showprob), pos = 1, col = "red")
+    cat("Probability", k, "and below =", this.prob, "\n")
+  }
+  if (!lower.tail) {
+    this.prob <- 1 - pbinom(k - 1, n, prob)
+    showprob <- format(this.prob, digits = 4)
+    lines(k:n, dbinom(k:n, size = n, prob), col = "red", type = "h", lwd = 2)
+    text((maxx + n * prob) * 9 / 16, myy, labels = paste("P(X \u2265", k, ")\n =", showprob), pos = 1, col = "red")
+    cat("Probability", k, "and above =", this.prob, "\n")
+  }
+  newtitle <- substitute(paste("Binomial (", n == x1, ", ", pi == x2, ")", ), list(x1 = n, x2 = prob))
+  title(newtitle)
+  mtext(side = 1, line = 2, "Number of Successes")
+  mtext(side = 2, line = 2, "Probability")
+
+  return(this.prob)
+}
+
+#' Exact Binomial Test
+#'
+#' `binomtest` calculates performas an exact binomial test and graphs the
+#'    binomial distribution and/or binomial confidence interval.
+#'
+#' @param observed The observed number of successes or sample proportion (assumed
+#'    to be proportion if value less than one.)
+#' @param n number of trials.
+#' @param hypothesized hypothesized probability of success.
+#' @param alternative Form of alternative ("less", "greater", or "two.sided").
+#' @param conf.level Confidence level for a two-sided confidence interval.
+#'
+#' @return P-value along with a plot of the binomial distribution and/or
+#'  binomial confidence interval.
+#' @export
+#' @importFrom graphics points
+#' @importFrom stats qbeta
+#'
+#' @examples
+binomtest <- function(observed, n, hypothesized = NULL, alternative, conf.level = NULL) {
+  # TODO Better documentation that takes into account all parts of description
+  Description <- "iscambinomtest(observed, n, hypothesized=NULL, alternative, conf.level=NULL) \n This function performs an exact binomial test and graphs the binomial distribution and/or binomial confidence interval.\n Input the observed number of successes or sample proportion (assumed if value less than one),\n Input n = the sample size and the hypothesized probability of success  \n Optional: Input the hypothesized probability of success and form of alternative (\"less\", \"greater\", or \"two.sided\") \n Optional: Input a confidence level (one or more values) for a two-sided confidence interval.\n "
+
+  if (as.character(observed) == "?") stop(Description)
+  par(mar = c(4, 3, 2, 2))
+
+  if (observed < 1) {
+    observed <- round(n * observed)
+  }
+  pvalue <- NULL
+  if (!is.null(hypothesized)) {
+    minx <- max(0, n * hypothesized - 4 * sqrt(hypothesized * (1 - hypothesized) * n))
+    maxx <- min(n, n * hypothesized + 4 * sqrt(hypothesized * (1 - hypothesized) * n))
+    maxx <- max(observed + 1, maxx)
+    myy <- max(dbinom(floor(n * hypothesized), n, hypothesized)) * .9
+    x <- 0:n
+    plot(x, dbinom(x, size = n, prob = hypothesized), xlab = "", ylab = " ", type = "h", xlim = c(minx, maxx), panel.first = grid(), lwd = 2)
+    newtitle <- substitute(paste("Binomial (", n == x1, ", ", pi == x2, ")", ), list(x1 = n, x2 = hypothesized))
+    title(newtitle)
+    mtext(side = 1, line = 2, "Number of Successes")
+    mtext(side = 2, line = 2, "Probability")
+
+    if (alternative == "less") {
+      pvalue <- pbinom(observed, size = n, prob = hypothesized, TRUE)
+      lines(0:observed, dbinom(0:observed, size = n, prob = hypothesized), col = "red", type = "h", lwd = 2)
+      text(minx, myy, labels = paste("p-value:", signif(pvalue, 4)), pos = 4, col = "red")
+    } else if (alternative == "greater") {
+      value <- observed - 1
+      pvalue <- pbinom(value, size = n, prob = hypothesized, FALSE)
+      lines(observed:n, dbinom(observed:n, size = n, prob = hypothesized), col = "red", type = "h", lwd = 2)
+      text(maxx, myy, labels = paste("p-value:", signif(pvalue, 4)), pos = 2, col = "red")
+    } else {
+      pvalue <- 0
+      firstvalue <- dbinom(observed, size = n, prob = hypothesized)
+      for (y in 0:n) {
+        newvalue <- dbinom(y, size = n, prob = hypothesized)
+        if (newvalue <= firstvalue + .00001) {
+          pvalue <- pvalue + newvalue
+          lines(y, newvalue, col = "red", type = "h", lwd = 2)
+        }
+      }
+      text(minx, myy, labels = paste("two-sided p-value:\n", signif(pvalue, 4)), pos = 4, col = "red")
+    }
+    pvalue <- signif(pvalue, 5)
+    abline(h = 0, col = "gray")
+    abline(v = 0, col = "gray")
+  }
+  cat("\n", "Exact Binomial Test\n", sep = "", "\n")
+  statistic <- signif(observed / n, 4)
+  cat(paste("Data: observed successes = ", observed, ", sample size = ", n, ", sample proportion = ", statistic, "\n\n", sep = ""))
+
+  if (!is.null(hypothesized)) {
+    cat(paste("Null hypothesis       : pi =", hypothesized, sep = " "), "\n")
+    altname <- switch(alternative,
+      less = "<",
+      greater = ">",
+      two.sided = "<>"
+    )
+    cat(paste("Alternative hypothesis: pi", altname, hypothesized, sep = " "), "\n")
+    cat(paste("p-value:", pvalue, sep = " "), "\n")
+  }
+  p.L <- function(x, alpha) {
+    if (x == 0) {
+      0
+    } else {
+      qbeta(alpha, x, n - x + 1)
+    }
+  }
+  p.U <- function(x, alpha) {
+    if (x == n) {
+      1
+    } else {
+      qbeta(1 - alpha, x + 1, n - x)
+    }
+  }
+  CINT <- 0
+  multconflevel <- 0
+  lower1 <- NULL
+  upper1 <- NULL
+  if (!is.null(conf.level)) {
+    for (k in 1:length(conf.level)) {
+      if (conf.level[k] > 1) conf.level[k] <- conf.level[k] / 100
+      alpha <- (1 - conf.level[k]) / 2
+      CINT <- c(signif(p.L(observed, alpha), 5), ",", signif(p.U(observed, alpha), 5))
+      multconflevel <- 100 * conf.level[k]
+      cat(multconflevel, "% Confidence interval for pi: (", CINT, ") \n")
+      lower1[k] <- as.numeric(CINT[1])
+      upper1[k] <- as.numeric(CINT[3])
+    }
+  }
+  par(mar = c(4, 2, 1.5, .5), mfrow = c(3, 1))
+  if (length(conf.level) > 1) par(mar = c(4, 2, 1.5, .4), mfrow = c(length(conf.level), 1))
+
+  if (is.null(hypothesized)) {
+    statistic <- observed / n
+    # lower=lower1[1]; upper=upper1[1]
+    SDphat <- sqrt(statistic * (1 - statistic) / n)
+    min <- statistic - 4 * SDphat
+    max <- statistic + 4 * SDphat
+    CIseq <- seq(min, max, .01)
+    minx <- as.integer(max(0, min * n))
+    maxx <- as.integer(min(n, max * n))
+
+    if (length(conf.level) == 1) {
+      myxlab <- substitute(paste("Binomial (", n == x1, ", ", pi == x2, ")", ), list(x1 = n, x2 = signif(lower1[1], 4)))
+      plot(seq(minx, maxx), dbinom(seq(minx, maxx), size = n, prob = lower1[1]), xlab = "  ", ylab = " ", type = "h", xlim = c(minx, maxx))
+      mtext("Number of successes", side = 1, line = 1.75, adj = .5, cex = .75)
+      title(myxlab)
+      lines(observed:n, dbinom(observed:n, size = n, prob = lower1[1]), col = "red", type = "h")
+
+      myxlab <- substitute(paste("Binomial (", n == x1, ", ", pi == x2, ")", ), list(x1 = n, x2 = signif(upper1[1], 4)))
+      plot(seq(minx, maxx), dbinom(seq(minx, maxx), size = n, prob = upper1[1]), xlab = " ", ylab = " ", type = "h", xlim = c(minx, maxx))
+      lines(0:observed, dbinom(0:observed, size = n, prob = upper1[1]), col = "red", type = "h")
+
+      mtext("Number of successes", side = 1, line = 1.75, adj = .5, cex = .75)
+      title(myxlab)
+    } # end only one interval
+
+    for (k in 1:length(conf.level)) {
+      plot(c(min, statistic, max), c(1, 1, 1), pch = c(".", "^", "."), ylab = " ", xlab = "process probability", ylim = c(1, 1))
+      abline(v = statistic, col = "gray")
+      text(min, 1, labels = paste(conf.level[k] * 100, "% CI:"))
+      text(statistic, .9, labels = signif(statistic, 4))
+      text(lower1[k], 1, labels = signif(lower1[k], 4), pos = 3)
+      text(upper1[k], 1, labels = signif(upper1[k], 4), pos = 3)
+      points(c(lower1[k], upper1[k]), c(1, 1), pch = c("[", "]"))
+      lines(c(lower1[k], upper1[k]), c(1, 1))
+    } # end intervals loop
+  } # end no hypothesized
+
+  par(mfrow = c(1, 1))
+  invisible(list("pvalue" = pvalue, "lower" = lower1, "upper" = upper1))
+}
+
+#' Inverse Binomial Probability
+#'
+#' @param alpha The probability of interest.
+#' @param n The number of trials.
+#' @param prob The probability of success.
+#' @param lower.tail Boolean for finding the probability above (FALSE) or below
+#'  (TRUE) the inputted value (inclusive)
+#'
+#' @return numeric which achieves at most the stated probability
+#' @export
+#'
+#' @examples
+invbinom <- function(alpha, n, prob, lower.tail) {
+  Description <- "iscaminvbinom(alpha, n, prob, lower.tail) \n This function calculates the binomial quantile of a specified probability. \n Input the desired probability and the parameters of the binomial distribution. \n Specify whether you want this is an upper tail (FALSE) or lower tail (TRUE) \n The integer that achieves at most the stated probability will be returned."
+
+  if (as.character(alpha) == "?") stop(Description)
+  par(mar = c(4, 3, 2, 2))
+
+  thisx <- 0:n
+  minx <- max(0, n * prob - 4 * sqrt(prob * (1 - prob) * n))
+  maxx <- min(n, n * prob + 4 * sqrt(prob * (1 - prob) * n))
+  myy <- dbinom(floor(n * prob), n, prob) / 2
+  plot(thisx, dbinom(thisx, size = n, prob), xlab = " ", ylab = " ", type = "h", xlim = c(minx, maxx), panel.first = grid(), lwd = 2)
+  newtitle <- substitute(paste("Binomial (", n == x1, ", ", pi == x2, ")", ), list(x1 = n, x2 = prob))
+  title(newtitle)
+  mtext(side = 1, line = 2, "X = Number of Successes")
+  mtext(side = 2, line = 2, "Probability")
+
+
+  abline(h = 0, col = "gray")
+  if (lower.tail) {
+    answer <- qbinom(alpha, n, prob, lower.tail) - 1
+    actualprob <- format(pbinom(answer, n, prob, lower.tail), digits = 4)
+    lines(0:answer, dbinom(0:answer, size = n, prob), col = "red", type = "h", lwd = 2)
+    text(x = minx, y = myy, labels = paste("P(X \u2264", answer, ")=", actualprob, "\nP(X \u2264", answer + 1, ")=", format(pbinom(answer + 1, n, prob, lower.tail), digits = 4)), col = "red", pos = 4)
+    # text(answer, dbinom(answer, size=n, prob), labels=actualprob, pos=2, col="red")
+    text(answer, dbinom(answer, size = n, prob), labels = paste("X=", answer), col = "red", pos = 3)
+    cat("The observation with at most", alpha, "probability at or below is", answer, "\n")
+  }
+  if (!lower.tail) {
+    answer <- qbinom(alpha, n, prob, lower.tail) + 1
+    actualprob <- format(pbinom(answer - 1, n, prob, lower.tail), digits = 4)
+    lines(answer:n, dbinom(answer:n, size = n, prob), col = "red", type = "h", lwd = 2)
+    myx <- (maxx + n * prob) / 2
+    text(x = myx, y = myy, labels = paste("P(X \u2265", answer, ")=", actualprob, "\n P(X \u2265", answer - 1, ")=", format(pbinom(answer - 2, n, prob, lower.tail), digits = 4)), col = "red")
+    # text(answer, dbinom(answer, size=n, prob), labels=actualprob, pos=2, col="red")
+    text(answer, dbinom(answer, n, prob), labels = paste("X=", answer), col = "red", pos = 3)
+    cat("The observation with at most", alpha, "probability at or above is", answer, "\n")
+  }
+  return(answer)
+}
+
+#' Title
+#'
+#' @param k
+#' @param prob
+#' @param n
+#' @param pi
+#' @param lower.tail Boolean for finding the probability above (FALSE) or below
+#' (TRUE) the inputted value (inclusive).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+binom <- function(k, prob, n, pi, lower.tail) {
+  # TODO Delete? Just use the two functions separately.
+  Description <- "iscambinom(k, prob, n, pi, lower.tail) \n This function calculates either a tail probability or the inverse cumulative probability depending on whether k or prob is passed a question mark."
+
+  if (as.character(prob) == "?") binomprob(k, n, pi, lower.tail)
+  if (as.character(k) == "?") invbinom(prob, n, pi, lower.tail)
 }
