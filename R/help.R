2f3b5282546c1e747431cc296aa53469b43bb7ca
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-01-20 19:29:31 -0800

    Cleaned up code a little

--- R/help.R
+++ R/help.R
@@ -1,6 +1,7 @@
 .iscam_format_help <- function(lines) {
-  lines <- gsub(".\b", "", lines)
-  lines <- gsub("[\u2018\u2019\u201c\u201d]", "'", lines)
+  lines <- lines |>
+    gsub(".\b", "", x = _) |>
+    gsub("[\u2018\u2019\u201c\u201d]", "'", x = _)
   formatted <- character(0)
   in_args <- FALSE
 
@@ -38,36 +39,40 @@
   sub("\\s+$", "", formatted)
 }
 
-.iscam_help_output <- function(rd_path) {
-  output <- capture.output(tools::Rd2txt(rd_path, out = ""))
-  .iscam_format_help(output)
-}
-
 .iscam_show_help <- function(topic) {
   pkg_man <- system.file("man", package = "ISCAM")
-  if (nzchar(pkg_man)) {
-    pkg_rd <- file.path(pkg_man, paste0(topic, ".Rd"))
-    if (file.exists(pkg_rd)) {
-      cat(paste(.iscam_help_output(pkg_rd), collapse = "\n"), "\n", sep = "")
-      return(invisible(TRUE))
-    }
+  pkg_rd <- if (nzchar(pkg_man)) {
+    file.path(pkg_man, paste0(topic, ".Rd"))
+  } else {
+    ""
+  }
+  if (nzchar(pkg_rd) && file.exists(pkg_rd)) {
+    capture.output(tools::Rd2txt(pkg_rd, out = "")) |>
+      .iscam_format_help() |>
+      paste(collapse = "\n") |>
+      cat("\n", sep = "")
+    return(invisible(TRUE))
   }
 
-  pkg_path <- NULL
-  if ("ISCAM" %in% loadedNamespaces()) {
-    pkg_path <- getNamespaceInfo("ISCAM", "path")
+  pkg_path <- if ("ISCAM" %in% loadedNamespaces()) {
+    getNamespaceInfo("ISCAM", "path")
+  } else {
+    NULL
   }
 
-  rd_candidates <- character(0)
-  if (!is.null(pkg_path)) {
-    rd_candidates <- c(rd_candidates, file.path(pkg_path, "man"))
-  }
-  rd_candidates <- c(rd_candidates, file.path(getwd(), "man"))
+  rd_path <- c(
+    if (!is.null(pkg_path)) file.path(pkg_path, "man"),
+    file.path(getwd(), "man")
+  ) |>
+    file.path(paste0(topic, ".Rd")) |>
+    (`[`)(file.exists(.)) |>
+    (\(paths) paths[1])()
 
-  rd_candidates <- file.path(rd_candidates, paste0(topic, ".Rd"))
-  rd_path <- rd_candidates[file.exists(rd_candidates)][1]
   if (!is.na(rd_path) && nzchar(rd_path)) {
-    cat(paste(.iscam_help_output(rd_path), collapse = "\n"), "\n", sep = "")
+    capture.output(tools::Rd2txt(rd_path, out = "")) |>
+      .iscam_format_help() |>
+      paste(collapse = "\n") |>
+      cat("\n", sep = "")
     return(invisible(TRUE))
   }
 
@@ -77,8 +82,12 @@
     return(invisible(FALSE))
   }
 
-  rd_file <- utils:::.getHelpFile(help_obj)
-  cat(paste(.iscam_help_output(rd_file), collapse = "\n"), "\n", sep = "")
+  utils:::.getHelpFile(help_obj) |>
+    tools::Rd2txt(out = "") |>
+    capture.output() |>
+    .iscam_format_help() |>
+    paste(collapse = "\n") |>
+    cat("\n", sep = "")
   invisible(TRUE)
 }
 
